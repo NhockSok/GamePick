754524b1da3e6de6f40336035a9a003856c8dc7d
Nguyễn Thành Nhân <nhocksok1009@gmail.com>
2019-10-22 17:26:23 +0700

    Game pickballs

---
+++ Player/Player/Controller.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Player
+{
+    class Controller
+    {
+        private Board Game;
+        private Player p1, p2;
+       
+        public Controller()
+        {
+            this.Game = new Board(3, 5, 7);
+            this.p1 = new Player("Nhân");
+            this.p2 = new Player("Alexandra Wut");
+        }
+        public void PlayGame()
+        {
+            Game.PrintGame();
+            while(true)
+            {
+                Console.WriteLine("Turn of "+p1.GetName());
+                p1.PickBall(Game);
+                Game.PrintGame();
+                if(Game.IsGameOver())
+                {
+                    Console.WriteLine("Player 1 Lose!!");
+                    break;
+                }
+                Console.WriteLine("It's turn"+p2.GetName());
+                p2.PickBall(Game);
+                Game.PrintGame();
+                if (Game.IsGameOver())
+                {
+                    Console.WriteLine("Player 2 Lose!!");
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ Player/Player/Player.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Player
+{
+    class Player
+    {
+        private string Name;
+
+        public void PickBall(Board Game)
+        {
+            Console.Write("Which group do you choose: ");
+            int group =int.Parse(Console.ReadLine());
+
+            Console.Write("How many balls do you pick: ");
+            int balls = int.Parse(Console.ReadLine());
+
+            Game.PickBall(group, balls);
+        }
+        
+        public Player(string name)
+        {
+            this.Name = name;
+        }
+        public string GetName()
+        {
+            return this.Name;
+        }
+    }
+}
